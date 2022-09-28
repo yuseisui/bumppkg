96fc1f375a65adfc9358a3b3096c234673310598
yuseisui <53548640+yuseisui@users.noreply.github.com>
2022-09-29 05:37:38 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# bumppkg
+
+Bump a package version like `npm version`
+
+> **Note**
+> npm users should use `npm version`.
+> Basically I created bumppkg to fulfill my own needs to do the same as `npm version` without npm.
+
+## Install
+
+```bash
+npm i -g bumppkg
+```
+
+You can also use without global installation:
+
+```bash
+yarn dlx bumppkg ... # for yarn
+pnpm dlx bumppkg ... # for pnpm
+```
+
+## Usage
+
+```bash
+bumppkg [options] <newversion>
+```
+
+See `bumppkg --help` for details.
+
+## License
+
+[MIT License](LICENSE)
---
+++ cli.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+import {program} from 'commander';
+import npmVersion from 'libnpmversion';
+
+program
+	.name('bumppkg')
+	.argument(
+		'<newversion>',
+		'<newversion> | major | minor | patch | premajor | preminor | prepatch | prerelease | from-git\nThe `newversion` argument should be a valid semver string, a valid second argument to semver.inc (one of `patch`, `minor`, `major`, `prepatch`, `preminor`, `premajor`, `prerelease`), or `from-git`. In the second case, the existing version will be incremented by 1 in the specified field. `from-git` will try to read the latest git tag, and use that as the new npm version.',
+	)
+	.option(
+		'--path <path>',
+		'The path to the package being versionified. Defaults to process.cwd().',
+	)
+	.option(
+		'--allow-same-version',
+		'Allow setting the version to the current version in package.json.',
+	)
+	.option(
+		'--preid <prerelease-id>',
+		'When the `newversion` is pre, premajor, preminor, or prepatch, this defines the prerelease string, like `beta` etc.',
+	)
+	.option(
+		'--tag-version-prefix <prefix>',
+		'The prefix to add to the raw semver string for the tag name. Defaults to `v`. (So, by default it tags as `v1.2.3` when versioning to 1.2.3.)',
+	)
+	.option('--no-commit-hooks', 'Do not run git commit hooks.')
+	.option('--no-git-tag-version', 'Do not tag the version.')
+	.option('--sign-git-commit', 'GPG sign the git commit.')
+	.option('--sign-git-tag', 'GPG sign the git tag.')
+	.option('--force', 'Push forward recklessly if any problems happen.')
+	.option(
+		'--ignore-scripts',
+		'Do not run pre/post/version lifecycle scripts.',
+	)
+	.option(
+		'--script-shell <path>',
+		'Path to the shell, which should execute the lifecycle scripts. Defaults to `/bin/sh` on unix, or `cmd.exe` on windows.',
+	)
+	.option(
+		'--message <message>',
+		'The message for the git commit and annotated git tag that are created.',
+	)
+	.option(
+		'--silent',
+		'Passed to @npmcli/run-script to control whether it logs.',
+	)
+	.action(async (newversion, options) => {
+		const version = await npmVersion(newversion, options);
+		console.log(version);
+	});
+
+program.parseAsync();
